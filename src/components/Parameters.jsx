ce2e37b5a041aed0d69ebddbf5d840c6fd515d14
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-22 15:12:50 -0300

    childrens components

--- src/components/Parameters.jsx
+++ src/components/Parameters.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default (param) =>
+export default (props) =>
     <>
-        <h3>Título: {param.titulo}</h3>
-        <h4>Subtítulo: {param.subtitulo}</h4>
+        <h3>Título: {props.titulo}</h3>
+        <h4>Subtítulo: {props.subtitulo}</h4>
+        <p>{Array(10).fill(0)}</p>
     </>
+
+    // e possivel usar codigo javascript dentro da tag 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import FirstComponent from './components/FirstComponent'
 import Parameters from './components/Parameters'
+import Childrens from './components/children'
+import children from './components/children'
 
 ReactDOM.render(
     <div>
-        <FirstComponent></FirstComponent>
+        {/* <FirstComponent></FirstComponent>
         <Parameters titulo="Hello"
-        subtitulo="world"></Parameters>
+        subtitulo="world"></Parameters> */}
+        <children>
+            <ul>
+                <li>Banana</li>
+                <li>Maça</li>
+                <li>Laranja</li>
+                <li>Teste</li>
+            </ul>
+        </children>
+
     </div>,
     document.getElementById("root")
 )
