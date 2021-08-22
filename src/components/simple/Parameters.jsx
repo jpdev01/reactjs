6740ffc022b0e87b59d0ae2254390dd6301d2856
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-22 18:11:13 -0300

    anotation

--- src/components/simple/Parameters.jsx
+++ src/components/simple/Parameters.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default (props) =>
+    //props é somente leitura. Isso gerara erro.
+    //props.title = "Alterando o valor do title"
     <>
         <h3>Título: {props.title}</h3>
         <h4>Subtítulo: {props.subtitle}</h4>
