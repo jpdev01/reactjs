e4684e0d1a8ed836a7d2133a0e4db28f06f75cf7
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-22 17:22:28 -0300

    consideracoes

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,31 @@ import Childrens from './components/children'
 import children from './components/children'
 import Card from './components/card/Card'
 
+import './App.css'
+
 
 export default props => (
-    <div>
+    <div className="App">
+        <Card titulo="Hello World">
+            <FirstComponent></FirstComponent>
+        </Card>
+        <Card titulo="Com Parametros">
+            <Parameters titulo="Hello"
+                subtitulo="world"></Parameters>
+        </Card>
 
-        {/* <FirstComponent></FirstComponent>
-        <Parameters titulo="Hello"
-        subtitulo="world"></Parameters> */}
 
-        {/* <children>
-            <ul>
-                <li>Banana</li>
-                <li>Maça</li>
-                <li>Laranja</li>
-                <li>Teste</li>
-            </ul>
-        </children> */}
+        <Card titulo="Com filhos">
+            <children>
+                <ul>
+                    <li>Banana</li>
+                    <li>Maça</li>
+                    <li>Laranja</li>
+                    <li>Teste</li>
+                </ul>
+            </children>
+        </Card>
+
 
         <Card titulo="Exercício 01">
             <FirstComponent></FirstComponent>
